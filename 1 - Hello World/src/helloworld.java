324e65f735360a1f9c5ca38e0d79436844445e33
Gabriel <gandrade4@alu.ufc.br>
2019-08-15 11:20:17 -0300

    Brincando com o HelloWorld, exercícios 2.14

--- 1 - Hello World/src/helloworld.java
+++ 1 - Hello World/src/helloworld.java
@@ -1,7 +1,14 @@
 public class helloworld {
     public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
         String a = "Hello";
         String b = "World";
         System.out.println(a + " " + b);
+
+        System.out.println("Fazendo quebra\nde linha");
+
+        System.out.println(a + b);
     }
 }
---
+++ 2 - Exercício de fixação de sintaxe/src/Questao_01.java
@@ -0,0 +1,9 @@
+public class Questao_01 {
+}
+
+    public static void main(String[] args) {
+        //Questão 01:  Imprima	todos	os	números	de	150	a	300.
+
+        for(int i = 150; i <= 300; i++);
+            System.out.println(i);
+    }
