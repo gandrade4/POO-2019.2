1c3866d9853fe8080ee8764d7a5493bb47017922
Gabriel <gandrade4@alu.ufc.br>
2019-08-16 12:10:37 -0300

    Questão 2 do exercício 3.14

--- 2 - Exercício de fixação de sintaxe/src/Questao_02.java
+++ 2 - Exercício de fixação de sintaxe/src/Questao_02.java
@@ -2,5 +2,12 @@ public class Questao_02 {
     public static void main(String[] args) {
         // Questão 2: Imprima a soma de 1 a 1000.
 
+        int soma = 0;
+
+        for(int i = 1; i < 1001; i++){
+            soma += i;
+        }
+        System.out.println(soma);
+
     }
 }
