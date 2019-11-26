d4dbd224bb5bb01137db38f1e64fed93b51d8318
Gabriel <gandrade4@alu.ufc.br>
2019-11-26 20:33:53 -0300

    Começando o projeto Matrícula

---
+++ 17 - Matrícula/src/Aluno.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Aluno {
+    private String idAluno;
+    List<Discp> discps;
+
+    public Aluno(){
+
+    }
+
+    public void matricular(Discp discp){
+
+    }
+    public void desmatricular
+}
